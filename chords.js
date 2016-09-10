5b3f7a51b21779645fc219f9ca755e9de320980d
frosted_fronks <ryzingsun11@yahoo.com>
2016-09-10 12:45:09 -0500

    Complete chord formulas

--- chords.js
+++ chords.js
@@ -6,12 +6,6 @@ function Chord(name) {
   // this.notes = new Tuning
 };
 
-/*
-Major:
-Minor: Cmin, Cmin7, Cmin9
-Dominant: C7, C9, C11, C13, Csus, Csus2, Csus4, Cdim
-*/ 
-
 Chord.prototype.name = function(chordName) {
   // click from list, don't have to worry Regexing user input
   // parse into array of strings
@@ -29,19 +23,42 @@ Chord.prototype.mapToFretBoard = function(first_argument) {
 Chord.prototype.decompose = function(chordName) {
   // JS constant for chord formulas
   const CHORD_FORMULAS = {
+    //major
     maj: ["1", "3", "5"],
-    min: ["1", "b3", "5"],
-    dim: ["1", "b3", "b5"],
-    aug: ["1", "3", "#5"],
-    maj7: ["1", "3", "5", "7"],
-    min7: ["1", "b3", "5", "b7"],
-    dim7: ["1", "3", "5", "6"],
-    aug7: ["1", "3", "#5", "b7"],
-    dom7: ["1", "3", "5", "b7"],
     5: ["1", "5"],
     6: ["1", "3", "5", "6"],
-    6/9: ["1", "3", "5", "6", "9"],
-
+    "6/9": ["1", "3", "5", "6", "9"],
+    maj7: ["1", "3", "5", "7"],
+    maj9: ["1", "3", "5", "7", "9"],
+    maj11: ["1", "3", "5", "7", "9", "11"],
+    maj13: ["1", "3", "5", "7", "9", "11", "13"],
+    add9: ["1", "3", "5", "9"],
+    //minor
+    min: ["1", "b3", "5"],
+    min6: ["1", "b3", "5", "6"],
+    min7: ["1", "b3", "5", "b7"],
+    minadd9: ["1", "b3", "5", "9"],
+    "min6/9": ["1", "b3", "5", "6", "9"],
+    min9: ["1", "b3", "5", "b7", "9"],
+    min11: ["1", "b3", "5", "b7", "9", "11"],
+    min13: ["1", "b3", "5", "b7", "9", "11", "13"],
+    //dominant
+    7: ["1", "3", "5", "b7"],
+    9: ["1", "3", "5", "b7", "9"],
+    maj11: ["1", "3", "5", "b7", "9", "11"],
+    maj13: ["1", "3", "5", "b7", "9", "11", "13"],
+    "7#5": ["1", "3", "#5", "b7"],
+    "7b5": ["1", "3", "b5", "b7"],
+    "7#9": ["1", "3", "5", "b7", "#9"],
+    "7b9": ["1", "3", "5", "b7", "b9"],
+    //symmetrical
+    dim: ["1", "b3", "b5"],
+    dim7: ["1", "3", "5", "6"],
+    aug: ["1", "3", "#5"],
+    aug7: ["1", "3", "#5", "b7"],
+    //miscellaneous
+    sus2: ["1", "2", "5"],
+    sus4: ["1", "4", "5"],
   };
 
   return CHORD_FORMULAS.chordName
