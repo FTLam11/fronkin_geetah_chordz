b4bc71766a6b8d9fe7b85b358112cf212eafae15
frosted_fronks <ryzingsun11@yahoo.com>
2016-09-18 11:39:45 -0500

    Change state url and routerProvider

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var geetah = angular.module('geetah', ['ui.router']);
 geetah.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('geetah', {
-      url: '/home',
+      url: '/',
       templateUrl: '/geetah/_geetah.html',
       controller: 'GeetahCtrl'
     });
 
-    // $urlRouterProvider.otherwise('geetah');
+    $urlRouterProvider.otherwise('/');
 }]);
