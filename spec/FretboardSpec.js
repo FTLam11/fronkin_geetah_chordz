cc0400ad66b4cd5d4ef67bac0b1e6edecddb0808
frosted_fronks <ryzingsun11@yahoo.com>
2016-09-18 23:43:35 -0500

    Add test to verify each string gets notes

--- spec/FretboardSpec.js
+++ spec/FretboardSpec.js
@@ -1,9 +1,17 @@
 describe("Fretboard", function() {
   describe("fretboard", function() {
-    it("returns a mapping of notes to each fret", function() {
+    beforeEach(function() {
       testBoard = new Fretboard(["E", "A", "D", "G", "B", "E"], ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"])
+    });
+
+    it("returns a mapping of notes to each fret for a string", function() {
       eString = testBoard.fretboard[0];
       expect(eString).toEqual(["E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#"]);
+    });
+
+    it("returns a mapping of notes to each fret for all six strings", function() {
+      numStrings = testBoard.fretboard.length
+      expect(numStrings).toEqual(6);
     });
   });
 });
