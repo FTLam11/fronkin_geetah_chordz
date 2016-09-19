99c9258967af3f99b3c638a10584ce3d356358e6
frosted_fronks <ryzingsun11@yahoo.com>
2016-09-18 23:04:05 -0500

    Display tuning on nut

--- geetah/geetahCtrl.js
+++ geetah/geetahCtrl.js
@@ -1,9 +1,16 @@
 var geetah = angular.module('geetah');
 
+geetah.filter('reverse', function() {
+  return function(notes) {
+    return notes.slice().reverse();
+  };
+});
+
 geetah.controller('GeetahCtrl', ['$scope', 'Tuning', function($scope, Tuning) {
-  console.log("Hi");
-  var standard = new Tuning('EADGBE', 'E');
-  $scope.tuning = standard.intervals;
+  var algernon = new Tuning('DAEAC#E', 'D');
+  $scope.openNotes = algernon.notes;
   $scope.root = '';
   $scope.chord = '';
 }]);
+
+// color code the notes 1..7 ROYGBIV
