58994930a9f9e2277faca1ec9e70dc2092516bcb
frosted_fronks <ryzingsun11@yahoo.com>
2016-10-04 00:22:41 -0500

    Move test setup to beforeEach

--- spec/ChordSpec.js
+++ spec/ChordSpec.js
@@ -1,14 +1,16 @@
 describe("Chord", function() {
+  beforeEach(function() {
+    testChord = new Chord("maj7", "C");
+  });
+
   describe("name", function() {
     it("returns the name of the chord", function() {
-      testChord = new Chord("maj7", "C");
       expect(testChord.name).toEqual("Cmaj7");
     });
   });
 
   describe("intervals", function() {
     it("returns the chord formula", function() {
-      testChord = new Chord("maj7", "C");
       expect(testChord.intervals).toEqual(["1", "3", "5", "7"]);
     });
   });
