5a4ec85d83256e427089d6f993eeb4009f601708
frosted_fronks <ryzingsun11@yahoo.com>
2016-10-04 00:22:18 -0500

    Add 7sus4 and 9sus4 to chord types

--- chords.js
+++ chords.js
@@ -1,10 +1,3 @@
-// 1 b2 2 b3 3 4 b5 5 #5 6 b7 7
-// name = $('#chordType').value()
-// chord = new Chord()
-
-// 1. Given a tuning show intervals/notes on fretboard
-// 2. Select chord to show intervals/notes for chord
-
 function Chord(type, root) {
   this.name = root + type;
   this.intervals = this.chordQuery(type);
@@ -20,12 +13,11 @@ Chord.prototype.mapToFretBoard = function(first_argument) {
   // body...
 };
 
-Chord.prototype.chordQuery = function(chordName) {
-  return CHORD_FORMULAS[chordName]
+Chord.prototype.chordQuery = function(type) {
+  return CHORD_FORMULAS[type];
 };
 
 const CHORD_FORMULAS = {
-  //major
   maj: ["1", "3", "5"],
   5: ["1", "5"],
   6: ["1", "3", "5", "6"],
@@ -35,7 +27,6 @@ const CHORD_FORMULAS = {
   maj11: ["1", "3", "5", "7", "9", "11"],
   maj13: ["1", "3", "5", "7", "9", "11", "13"],
   add9: ["1", "3", "5", "9"],
-  //minor
   min: ["1", "b3", "5"],
   min6: ["1", "b3", "5", "6"],
   min7: ["1", "b3", "5", "b7"],
@@ -44,7 +35,6 @@ const CHORD_FORMULAS = {
   min9: ["1", "b3", "5", "b7", "9"],
   min11: ["1", "b3", "5", "b7", "9", "11"],
   min13: ["1", "b3", "5", "b7", "9", "11", "13"],
-  //dominant
   7: ["1", "3", "5", "b7"],
   9: ["1", "3", "5", "b7", "9"],
   maj11: ["1", "3", "5", "b7", "9", "11"],
@@ -53,12 +43,12 @@ const CHORD_FORMULAS = {
   "7b5": ["1", "3", "b5", "b7"],
   "7#9": ["1", "3", "5", "b7", "#9"],
   "7b9": ["1", "3", "5", "b7", "b9"],
-  //symmetrical
   dim: ["1", "b3", "b5"],
   dim7: ["1", "3", "5", "6"],
   aug: ["1", "3", "#5"],
   aug7: ["1", "3", "#5", "b7"],
-  //miscellaneous
   sus2: ["1", "2", "5"],
   sus4: ["1", "4", "5"],
+  "7sus4": ["1", "4", "5", "b7"],
+  "9sus4": ["1", "4", "5", "b7", "9"]
 };
