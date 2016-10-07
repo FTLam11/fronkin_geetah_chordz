dc99fb2c6fda81f2641e600918332ca61d02c81f
frosted_fronks <ryzingsun11@yahoo.com>
2016-10-06 21:50:25 -0500

    Prepare app for githubpages

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var geetah = angular.module('geetah', ['ui.router', 'ngAnimate']);
 
-geetah.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+geetah.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
   $stateProvider
     .state('geetah', {
       url: '/',
       templateUrl: '/geetah/_geetah.html',
       controller: 'GeetahCtrl'
     });
+
+    $locationProvider.html5Mode(true);
 
     $urlRouterProvider.otherwise('/');
 }]);
