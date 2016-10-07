c2a4864bf5996d625bb47bac8ec44e9ab19dbaad
frosted_fronks <ryzingsun11@yahoo.com>
2016-10-07 10:08:56 -0500

    Move procedure into nested loop for getNote()

--- chords.js
+++ chords.js
@@ -21,16 +21,16 @@ Chord.prototype.getNotes = function(intervals) {
   var intervalNoteArr = [];
 
   for (var i = 0; i < INTERVAL_SYMBOLS.length; i++) {
-    var noteObj = {};
-    noteObj['note'] = [notes[currentNoteIndex]];
-    noteObj['interval'] = INTERVAL_SYMBOLS[i];
-
-    if (notes[currentNoteIndex].match(/.{1}#/) != null) {
-      noteObj['note'].push(SHARP_TO_FLAT[notes[currentNoteIndex]]);
-    };
-
     for (var j = 0; j < intervals.length; j++) {
-      if (noteObj['interval'] == intervals[j]) {
+      if (INTERVAL_SYMBOLS[i] == intervals[j]) {
+        var noteObj = {};
+        noteObj['note'] = [notes[currentNoteIndex]];
+        noteObj['interval'] = INTERVAL_SYMBOLS[i];
+      
+        if (notes[currentNoteIndex].match(/.{1}#/) != null) {
+          noteObj['note'].push(SHARP_TO_FLAT[notes[currentNoteIndex]]);
+        };
+
         intervalNoteArr.push(noteObj);
       };
     };
